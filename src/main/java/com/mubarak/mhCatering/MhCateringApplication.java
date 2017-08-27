6c0750dc6434a9a4cc6abc176e296eb0cb120b80
ibrezsyed <ibrezsyed@gmail.com>
2017-08-27 19:09:29 +0530

    First commit

---
+++ src/main/java/com/mubarak/mhCatering/MhCateringApplication.java
@@ -0,0 +1,12 @@
+package com.mubarak.mhCatering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhCateringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhCateringApplication.class, args);
+	}
+}
