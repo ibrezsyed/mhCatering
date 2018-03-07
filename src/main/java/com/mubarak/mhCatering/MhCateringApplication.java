7ed1d51b7cffd8e7daec935143c2c395c9a48b1a
syed <ibrezsyed>
2018-03-07 17:24:07 +0530

    Added Println

--- src/main/java/com/mubarak/mhCatering/MhCateringApplication.java
+++ src/main/java/com/mubarak/mhCatering/MhCateringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MhCateringApplication {
 
 	public static void main(String[] args) {
+		System.out.println("MhCatering App");
 		SpringApplication.run(MhCateringApplication.class, args);
 	}
 }
